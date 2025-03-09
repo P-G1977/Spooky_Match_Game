2183d29b325521e9ae83df9f26716f560daee083
P-G1977 <30094530@student.ekcgroup.ac.uk>
2025-03-09 11:11:15 +0000

    storing javascript refs to my html elements

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+const moves = document.getElementById("moves-counter");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector("game-container");
+const result = document.getElementById("results");
+const controls = document.querySelector("controls-container");
