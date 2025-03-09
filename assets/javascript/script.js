3ba77d8aa431c0a6b3cdb64add0d165ba2ec01be
P-G1977 <30094530@student.ekcgroup.ac.uk>
2025-03-09 11:19:41 +0000

    created javascript items array for cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,24 @@ const stopButton = document.getElementById("stop");
 const gameContainer = document.querySelector("game-container");
 const result = document.getElementById("results");
 const controls = document.querySelector("controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+// items array
+const items = [
+    {name: "bat", image:"assets/images/bat.png"},
+    {name: "black-cat", image:"assets/images/black-cat.png"},
+    {name: "cauldron", image:"assets/images/cauldron.png"},
+    {name: "blood", image:"assets/images/blood.png"},
+    {name: "death", image:"assets/images/death.png"},
+    {name: "frankenstein", image:"assets/images/frankenstein.png"},
+    {name: "potion", image:"assets/images/potion.png"},
+    {name: "trident", image:"assets/images/trident.png"},
+    {name: "witch", image:"assets/images/witch.png"},
+    {name: "dracula", image:"assets/images/dracula.png"},
+    {name: "zombie", image:"assets/images/zombie.png"},
+    {name: "grave", image:"assets/images/grave.png"},
+];
+
