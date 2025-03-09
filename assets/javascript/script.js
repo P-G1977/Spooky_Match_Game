75d1a94b053c19614c0e8c6d5df4e0b1f206235b
P-G1977 <30094530@student.ekcgroup.ac.uk>
2025-03-09 13:08:08 +0000

    changed some html elements and javascript, having problems with files, trying to troubleshoot

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,9 @@
-const moves = document.getElementById("moves-counter");
+const moves = document.getElementById("moves-count");
 const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const gameContainer = document.querySelector("game-container");
-const result = document.getElementById("results");
+const result = document.getElementById("result");
 const controls = document.querySelector("controls-container");
 let cards;
 let interval;
